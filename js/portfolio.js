6d25034ac4970f159609c0ffe7e97cf60cfe42b2
Phillip Zhou <pzhou53@gmail.com>
2016-11-15 02:56:43 -0800

    Added noConflict line to the Javascript file so that other Javascript libraries that make use of the '$' symbol, but don't use jQuery do not cause conflicts.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,138 +1,144 @@
 // Scripts for Phillip's Portfolio.
 
-/* When the menu-open element in the sidebar container is clicked, the
- * sidebar will open, covering up the menu-open element. Also, if any sections
- * within the sidebar are clicked, the menu will close. */
-$("#menu-toggle, .sidebar-nav li a").click(function (event) {
-    event.preventDefault();
+// Needed to avoid conflict with other Javascript libraries.
+$.noConflict();
+jQuery(document).ready(function ($) {
+    /* When the menu-open element in the sidebar container is clicked, the
+     * sidebar will open, covering up the menu-open element. Also, if any sections
+     * within the sidebar are clicked, the menu will close. */
+    $("#menu-toggle, .sidebar-nav li a").click(function (event) {
+        event.preventDefault();
 
-    // Changes the images of the toggle button.
-    $("#toggle-right").toggleClass("active");
-    $("#toggle-left").toggleClass("active");
+        // Changes the images of the toggle button.
+        $("#toggle-right").toggleClass("active");
+        $("#toggle-left").toggleClass("active");
 
-    // Allows the sidebar to not be transparent.
-    $("#sidebar-container").toggleClass("active");
+        // Allows the sidebar to not be transparent.
+        $("#sidebar-container").toggleClass("active");
 
-    // Enables the hyperlink elements inside the sidebar.
-    $(".sidebar-nav li a").toggleClass("active");
-    $(".sidebar-nav li a").toggleClass("disabled");
+        // Enables the hyperlink elements inside the sidebar.
+        $(".sidebar-nav li a").toggleClass("active");
+        $(".sidebar-nav li a").toggleClass("disabled");
 
-    // Determines the color of the toggle button.
-    $("#menu-toggle").toggleClass("menu-active");
-    $("#menu-toggle").toggleClass("menu-inactive");
-});
+        // Determines the color of the toggle button.
+        $("#menu-toggle").toggleClass("menu-active");
+        $("#menu-toggle").toggleClass("menu-inactive");
+    });
 
-/* This function will move the user to the clicked section. The function listens to
- * all <a> elements that have href attrib that has a '#' in it. Leave out any
- * elements that have exactly '#' as href, or any of the other [attrib].*/
-$('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function (event) {
-    /* Find first '/' symbol in string, replace with ''. */
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        || location.hostname == this.hostname) {
-        var target = $(this.hash);
-        // Takes the last element of the hash, creates a new string, tries to find element with that ID.
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Move the user view to the target page's top offset over the duration of 1 second.
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
+    /* This function will move the user to the clicked section. The function listens to
+     * all <a> elements that have href attrib that has a '#' in it. Leave out any
+     * elements that have exactly '#' as href, or any of the other [attrib].*/
+    $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function (event) {
+        /* Find first '/' symbol in string, replace with ''. */
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            || location.hostname == this.hostname) {
+            var target = $(this.hash);
+            // Takes the last element of the hash, creates a new string, tries to find element with that ID.
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Move the user view to the target page's top offset over the duration of 1 second.
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
         }
+    });
+
+    // Status on if the to-top button is being shown - true if it is shown.
+    var FixedTop = false;
+    var FixedBottom = false;
+
+    var DocumentBottomLoc = $(document).height() - $(window).height();
+
+    /* This function determines, based on how far down the user scrolled, whether or
+     * not to show a button that allows the user to move back to the top. */
+    $(document).scroll(function (event) {
+        /* To-top button will not show until scroll distance is more than 250 units down. 
+         * But, show the to-next button. */
+        if ($(this).scrollTop() <= 250) {
+            // Disable to-top button if showing.
+            if (FixedTop) {
+                FixedTop = false;
+                $("#to-top").fadeOut(250, function () {
+                    $("#to-top").css({
+                        display: "none"
+                    });
+                });
+            }
+
+            // Enable to-next button if not showing.
+            if (!FixedBottom) {
+                FixedBottom = true;
+                $("#to-next").fadeIn(250, function () {
+                    $("#to-next").css({
+                        display: "block"
+                    });
+                });
+            }
+        }
+            /* Middle of the document. */
+        else if ($(this).scrollTop() > 250 && (DocumentBottomLoc - $(window).scrollTop()) > 250) {
+            // Enable to-top button if not showing.
+            if (!FixedTop) {
+                FixedTop = true;
+                $("#to-top").fadeIn(250, function () {
+                    $("#to-top").css({
+                        display: "block"
+                    });
+                });
+            }
+
+            // Enable to-next button if not showing.
+            if (!FixedBottom) {
+                FixedBottom = true;
+
+                $("#to-top").animate({
+                    bottom: "9%"
+                }, 250);
+
+                $("#to-next").fadeIn(500, function () {
+                    $("#to-next").css({
+                        display: "block"
+                    });
+                });
+            }
+        }
+            /* End of the document. Hide the to-next button. */
+        else {
+            // Enable to-top button if not showing.
+            if (!FixedTop) {
+                FixedTop = true;
+                $("#to-top").fadeIn(200, function () {
+                    $("#to-top").css({
+                        display: "block"
+                    });
+                });
+            }
+
+            // Disable the to-next button if showing.
+            if (FixedBottom) {
+                FixedBottom = false;
+                $("#to-next").fadeOut(250, function () {
+                    $("#to-next").css({
+                        display: "none"
+                    });
+                });
+
+                $("#to-top").animate({
+                    bottom: "2%"
+                }, 500);
+            }
+        }
+    });
+
+    /* Switches the images when a mouse hovers over the profile image. */
+    $(".image-center").mouseover(ChangePicture);
+    $(".image-center").mouseout(ChangePicture);
+
+    /* Listener function for the image switching effect. */
+    function ChangePicture(event) {
+        $("#switch-image").toggleClass("image-active");
     }
 });
 
-// Status on if the to-top button is being shown - true if it is shown.
-var FixedTop = false;
-var FixedBottom = false;
-
-var DocumentBottomLoc = $(document).height() - $(window).height();
-
-/* This function determines, based on how far down the user scrolled, whether or
- * not to show a button that allows the user to move back to the top. */
-$(document).scroll(function (event) {
-    /* To-top button will not show until scroll distance is more than 250 units down. 
-     * But, show the to-next button. */
-    if ($(this).scrollTop() <= 250) {
-        // Disable to-top button if showing.
-        if (FixedTop) {
-            FixedTop = false;
-            $("#to-top").fadeOut(250, function () {
-                $("#to-top").css({
-                    display: "none"
-                });
-            });
-        }
-
-        // Enable to-next button if not showing.
-        if (!FixedBottom) {
-            FixedBottom = true;
-            $("#to-next").fadeIn(250, function () {
-                $("#to-next").css({
-                    display: "block"
-                });
-            });
-        }
-    }
-        /* Middle of the document. */
-    else if ($(this).scrollTop() > 250 && (DocumentBottomLoc - $(window).scrollTop()) > 250) {
-        // Enable to-top button if not showing.
-        if (!FixedTop) {
-            FixedTop = true;
-            $("#to-top").fadeIn(250, function () {
-                $("#to-top").css({
-                    display: "block"
-                });
-            });
-        }
-
-        // Enable to-next button if not showing.
-        if (!FixedBottom) {
-            FixedBottom = true;
-
-            $("#to-top").animate({
-                bottom: "9%"
-            }, 250);
-
-            $("#to-next").fadeIn(500, function () {
-                $("#to-next").css({
-                    display: "block"
-                });
-            });
-        }
-    }
-        /* End of the document. Hide the to-next button. */
-    else {
-        // Enable to-top button if not showing.
-        if (!FixedTop) {
-            FixedTop = true;
-            $("#to-top").fadeIn(200, function () {
-                $("#to-top").css({
-                    display: "block"
-                });
-            });
-        }
-
-        // Disable the to-next button if showing.
-        if (FixedBottom) {
-            FixedBottom = false;
-            $("#to-next").fadeOut(250, function () {
-                $("#to-next").css({
-                    display: "none"
-                });
-            });
-
-            $("#to-top").animate({
-                bottom: "2%"
-            }, 500);
-        }
-    }
-});
-
-/* Switches the images when a mouse hovers over the profile image. */
-$(".image-center").mouseover(ChangePicture);
-$(".image-center").mouseout(ChangePicture);
-
-function ChangePicture(event) {
-    $("#switch-image").toggleClass("image-active");
-}
